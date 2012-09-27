417bba09ea75c5ae2719a704b18d2998edaddd0d
Paul Iannazzo <somethingitalian@gmail.com>
2012-09-27 11:07:07 -0400

    fixed error in couchapp file. there was not module.exports

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-var couchapp = require('couchapp')
-  , path = require('path')
-  ;
+var couchapp = require('couchapp'),
+path = require('path');
 
-ddoc = 
-  { _id:'_design/app'
-/*  , rewrites : 
-    [ {from:"/", to:'index.html'}
-    , {from:"/api", to:'../../'}
-    , {from:"/api/*", to:'../../*'}
-    , {from:"/*", to:'*'}
-    ]*/
-  }
-  ;
-
-ddoc.views = {};
-
-ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {   
-  if (newDoc._deleted === true && userCtx.roles.indexOf('_admin') === -1) {
-    throw "Only admin can delete documents on this database.";
-  } 
-}
-
+ddoc = {
+    _id: '_design/app'
+};
 couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'));
+module.exports = ddoc;
