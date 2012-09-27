50e5e005017841f14e08b96b845cad6c628f8b7d
Paul Iannazzo <somethingitalian@gmail.com>
2012-09-27 14:57:23 -0400

    got logging in working

--- attachments/test/couchdb_user_model.js
+++ attachments/test/couchdb_user_model.js
@@ -1,3 +1,14 @@
+function _throw(things){
+  return function(){
+    throw things;
+  }
+}
+function   call(done){
+  return function(){
+    done()
+  }
+}
+
 describe('mocha/chai',function(){
   it('testing tests',function(){ 
     assert.typeOf('foo','string','foo is string');
@@ -14,9 +25,6 @@ describe('user model',function(){
   it('should exist',function(){ 
     expect(Backbone).to.have.property('CouchDB_User');
   });
-});
-
-describe('user', function() {
   var user_pass = $.couch.newUUID();
   var user_name = $.couch.newUUID();
   
@@ -29,16 +37,41 @@ describe('user', function() {
       $.couch.userDb(
         function(user_db){
           user_db.openDoc("org.couchdb.user:"+user_name, {
-            success:function(user){
-              done();
-	    },
-            error:function(){
-              throw "user doesn't exist'";
-            }
+            success:call(done),
+            error:_throw( "user doesn't exist")
           })
         })
     }
     user.on('registered',make_sure_user_was_added);
     user.signup();
   })
+
+  it('should be able to login', function(done) {
+    var user = new Backbone.CouchDB_User({
+      name:user_name,
+      password:user_pass
+    });
+    function make_sure_user_was_loggedin() {
+      $.couch.session({
+        success: call(done),
+        error: _throw( 'session failed')
+      });
+    }
+    user.on('loggedin', make_sure_user_was_loggedin);
+    user.login();
+  });
+  
+  after(function(){
+    // cleanup: remove registered user, must be logged out (admin
+    // party mode)
+    $.couch.userDb(
+      function(user_db){
+        user_db.openDoc("org.couchdb.user:"+user_name, {
+          success:function(userDoc){
+            user_db.removeDoc(_.pick(userDoc,'_id','_rev'));
+	  },
+          error:_throw( "user doesn't exist")
+        })
+      })
+  })      
 })
